11c483636c37908766c6e1c8bac8366e05f5ba81
Momar Mbengue <momarious@gmail.com>
2021-09-08 23:56:27 +0000

    Commit 35

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -26,13 +26,14 @@ class ProductController extends AbstractController
      */
     public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
         $search = new Search();
         $form = $this->createForm(SearchType::class, $search);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
         }
 
         return $this->render('product/index.html.twig', [
